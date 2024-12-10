861f489aa7b39ccc697295d2326d72cdb913ab2d
hieutitz <nt.hieu616@gmail.com>
2024-12-10 10:23:28 +0700

    Add files via upload

---
+++ codeC-ss12-ex2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void inMang(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Nhap so luong phan tu cua mang: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Cac phan tu trong mang la: ");
+    inMang(arr, n);
+
+    return 0;
+}
+
